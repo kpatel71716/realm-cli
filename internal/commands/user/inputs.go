312be85435659f345e2f669a5355684d1bddcdc6
kpatel71716 <kush.patel@mongodb.com>
2021-02-03 15:31:23 -0600

    REALMC-7160 create common selectUsers function

--- internal/commands/user/inputs.go
+++ internal/commands/user/inputs.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/10gen/realm-cli/internal/cloud/realm"
 	"github.com/10gen/realm-cli/internal/terminal"
+
+	"github.com/AlecAivazis/survey/v2"
 )
 
 const (
@@ -46,4 +48,34 @@ func displayUserData(apt realm.AuthProviderType, user realm.User) string {
 		return fmt.Sprint(val)
 	}
 	return ""
+}
+
+func selectUsers(action string, ui terminal.UI, foundUsers []realm.User) ([]realm.User, error) {
+	selectableUsers := map[string]realm.User{}
+	selectableUserOptions := make([]string, len(foundUsers))
+	for idx, user := range foundUsers {
+		var apt realm.AuthProviderType
+		if len(user.Identities) > 0 {
+			apt = user.Identities[0].ProviderType
+		}
+		opt := displayUser(apt, user)
+		selectableUserOptions[idx] = opt
+		selectableUsers[opt] = user
+	}
+	var selectedUsers []string
+	askErr := ui.AskOne(
+		&selectedUsers,
+		&survey.MultiSelect{
+			Message: fmt.Sprintf("Which user(s) would you like to %s?", action),
+			Options: selectableUserOptions,
+		},
+	)
+	if askErr != nil {
+		return nil, askErr
+	}
+	users := make([]realm.User, len(selectedUsers))
+	for idx, user := range selectedUsers {
+		users[idx] = selectableUsers[user]
+	}
+	return users, nil
 }
